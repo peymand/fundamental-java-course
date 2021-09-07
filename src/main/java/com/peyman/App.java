439025bb58ce86bc30fd82aa16272f37b635f6eb
Peyman Dodangeh <peymandodangeh@gmail.com>
2021-09-07 17:02:19 +0430

    project ini

---
+++ src/main/java/com/peyman/App.java
@@ -0,0 +1,15 @@
+package com.peyman;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+        //TODO: check later
+    }
+}
